302f8a6790214208e5ad0f4b07bd617278f8b5ab
ruben rodriguez <leizan89@hotmail.com>
2019-05-07 20:02:41 +0200

    actualizar imagenes en proceso

--- app/src/main/java/com/example/proyectdam/Vista/Activity/Activity_Menu.java
+++ app/src/main/java/com/example/proyectdam/Vista/Activity/Activity_Menu.java
@@ -1,9 +1,11 @@
 package com.example.proyectdam.Vista.Activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -12,10 +14,28 @@ import com.example.proyectdam.Controlador.Fragments.C_Fragment_Menu;
 import com.example.proyectdam.Controlador.IntentsMenu;
 import com.example.proyectdam.R;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.HashMap;
+
 public class Activity_Menu extends AppCompatActivity {
     public C_Activity_Menu c_activity_menu;
     public C_Fragment_Menu c_fragment_menu;
     public String tag_escogido;
+
+    static final String HOST = "192.168.43.95";
+    static final int PUERTO = 5000;
+    InputStream is;
+    OutputStream os;
+    DataOutputStream envia;
+    ObjectInputStream recibir;
+    private HashMap radiocontrol = new HashMap();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +45,7 @@ public class Activity_Menu extends AppCompatActivity {
         c_activity_menu =  new C_Activity_Menu();
         c_fragment_menu = new C_Fragment_Menu();
         c_activity_menu.initialite();
+
     }
 
     @Override
@@ -53,6 +74,57 @@ public class Activity_Menu extends AppCompatActivity {
             Toast.makeText(myContext, "No tienes permisos para entrar a ese sitio.", Toast.LENGTH_SHORT).show();
         }
     }
+
+    public void ActualizarImagenes(View vista){
+
+        Socket skCliente = null;
+
+
+        try {
+            skCliente = new Socket(HOST,PUERTO);
+            is =  skCliente.getInputStream();
+            recibir =  new ObjectInputStream(is);
+            os =skCliente.getOutputStream();
+
+          //recibimos el numero de categorias que recibiremos.
+            int numCategorias = recibir.readInt();
+
+            for (int i=0;i<numCategorias;i++) {
+                String categoria = recibir.readUTF();
+
+                //creamos el file con la ruta donde se pondran las categorias y los archivos.
+                File rutaCategoria = new File(getApplicationContext().getFilesDir().getPath()+"\\Productos\\"+categoria);
+                 //creo las carpetas correspondientes;
+                  rutaCategoria.mkdirs();
+                radiocontrol= (HashMap) recibir.readObject();
+
+                //teno que hacer  fileoutputstram con la ruta qu ele corresponde para guardar las imagenes
+              //  FileOutputStream guardar = new FileOutputStream(DirProductos + "\\" + categoria);
+              //  guardar.write(imagen);
+
+                //metemos las imagenes del hashmap en la carpeta.
+
+                for(int j=0; j<radiocontrol.size();j++){
+
+
+
+
+
+                }
+
+
+            }
+
+
+
+        } catch (IOException e ) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
     private static Activity_Menu myContext;
     public Activity_Menu() { myContext = this; }
