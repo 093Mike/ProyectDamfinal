0de2402a1ab5d4596b445e1811cde4380f239bda
ruben rodriguez <leizan89@hotmail.com>
2019-05-08 16:26:12 +0200

    actualizar imagenes en proceso 2

--- app/src/main/java/com/example/proyectdam/Vista/Activity/Activity_Menu.java
+++ app/src/main/java/com/example/proyectdam/Vista/Activity/Activity_Menu.java
@@ -17,12 +17,14 @@ import com.example.proyectdam.R;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Activity_Menu extends AppCompatActivity {
     public C_Activity_Menu c_activity_menu;
@@ -35,99 +37,108 @@ public class Activity_Menu extends AppCompatActivity {
     OutputStream os;
     DataOutputStream envia;
     ObjectInputStream recibir;
-    private HashMap radiocontrol = new HashMap();
+    private HashMap<String, byte[]> dirCategoria = new HashMap();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         getSupportActionBar().hide();
-        c_activity_menu =  new C_Activity_Menu();
+        c_activity_menu = new C_Activity_Menu();
         c_fragment_menu = new C_Fragment_Menu();
         c_activity_menu.initialite();
 
     }
 
     @Override
-    public void onBackPressed()
-    {
+    public void onBackPressed() {
         super.onBackPressed();
         MainActivity.getInstance().c_activityMain.setControl(false);
     }
 
 
-    public void gestioMenu(View view){
+    public void gestioMenu(View view) {
         tag_escogido = ((String) view.getTag()).toUpperCase();
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         IntentsMenu intentsMenu = new IntentsMenu();
-        ft.replace(R.id.fragment_global, intentsMenu.gestioIntent_Menu(tag_escogido) , "fragment_meters" );
+        ft.replace(R.id.fragment_global, intentsMenu.gestioIntent_Menu(tag_escogido), "fragment_meters");
         ft.commit();
     }
 
-    public void gestioaddUsers(View view){
-        if(c_fragment_menu.entrarAddUsers()) {
+    public void gestioaddUsers(View view) {
+        if (c_fragment_menu.entrarAddUsers()) {
             String tag = (String) view.getTag();
             IntentsMenu intentsMenu = new IntentsMenu();
             startActivity(new Intent(this, MenuUser_add.class));
-        }
-        else{
+        } else {
             Toast.makeText(myContext, "No tienes permisos para entrar a ese sitio.", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void ActualizarImagenes(View vista){
+    public void ActualizarImagenes(View vista) {
 
-        Socket skCliente = null;
-
-
-        try {
-            skCliente = new Socket(HOST,PUERTO);
-            is =  skCliente.getInputStream();
-            recibir =  new ObjectInputStream(is);
-            os =skCliente.getOutputStream();
-
-          //recibimos el numero de categorias que recibiremos.
-            int numCategorias = recibir.readInt();
-
-            for (int i=0;i<numCategorias;i++) {
-                String categoria = recibir.readUTF();
-
-                //creamos el file con la ruta donde se pondran las categorias y los archivos.
-                File rutaCategoria = new File(getApplicationContext().getFilesDir().getPath()+"\\Productos\\"+categoria);
-                 //creo las carpetas correspondientes;
-                  rutaCategoria.mkdirs();
-                radiocontrol= (HashMap) recibir.readObject();
-
-                //teno que hacer  fileoutputstram con la ruta qu ele corresponde para guardar las imagenes
-              //  FileOutputStream guardar = new FileOutputStream(DirProductos + "\\" + categoria);
-              //  guardar.write(imagen);
-
-                //metemos las imagenes del hashmap en la carpeta.
-
-                for(int j=0; j<radiocontrol.size();j++){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Socket skCliente = null;
 
 
+                try {
+                    skCliente = new Socket(HOST, PUERTO);
+                    is = skCliente.getInputStream();
+                    recibir = new ObjectInputStream(is);
+                    os = skCliente.getOutputStream();
+
+                    //borramos la carpeta productos con todas las categorias dentro
+
+                    File rutaProductos = new File(getApplicationContext().getFilesDir()
+                            .getPath() + "\\Productos");
+                    rutaProductos.delete();
+
+                    //recibimos el numero de categorias que recibiremos.
+                    int numCategorias = recibir.readInt();
+
+                    for (int i = 0; i < numCategorias; i++) {
+                        String categoria = recibir.readUTF();
+
+                        //creamos el file con la ruta donde se pondran las categorias y los archivos.
+                        File rutaCategoria = new File(getApplicationContext().getFilesDir()
+                                .getPath() + "\\Productos\\" + categoria);
+                        //creo las carpetas correspondientes;
+                        rutaCategoria.mkdirs();
+                        dirCategoria = (HashMap) recibir.readObject();
 
 
+                        //metemos las imagenes del hashmap en la carpeta.
+                        for (Map.Entry<String, byte[]> entrada : dirCategoria.entrySet()) {
 
+                            //creamos   fileoutputstram con la ruta qu ele corresponde para guardar las imagenes
+                            FileOutputStream guardar = new FileOutputStream(rutaCategoria + "\\" + entrada.getKey());
+                            guardar.write(entrada.getValue());
+
+                        }
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
                 }
-
-
             }
+        }).start();
 
-
-
-        } catch (IOException e ) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
 
     }
 
 
     private static Activity_Menu myContext;
-    public Activity_Menu() { myContext = this; }
-    public static Activity_Menu getInstance() { return myContext; }
+
+    public Activity_Menu() {
+        myContext = this;
+    }
+
+    public static Activity_Menu getInstance() {
+        return myContext;
+    }
 
 }
