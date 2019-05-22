e696fc4d5e83901106c17ac2dd2b46125e968b1e
Javier Luque <javiluque.s@gmail.com>
2019-05-22 19:55:12 +0200

    Commit 1954

--- app/src/main/java/com/example/proyectdam/Vista/Activity/Activity_AddIncidencia.java
+++ app/src/main/java/com/example/proyectdam/Vista/Activity/Activity_AddIncidencia.java
@@ -3,6 +3,7 @@ package com.example.proyectdam.Vista.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -33,6 +34,7 @@ public class Activity_AddIncidencia extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_incidencias);
         setTitle("Activity_AddIncidencia");
 
