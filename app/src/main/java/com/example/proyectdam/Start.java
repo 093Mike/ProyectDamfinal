7a73522a5943ef14d8a7c2683591394740c59116
Mike <sackmike.093@gmail.com>
2019-05-24 17:44:53 +0200

    0.6.0

---
+++ app/src/main/java/com/example/proyectdam/Start.java
@@ -0,0 +1,17 @@
+package com.example.proyectdam;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class Start extends AppCompatActivity {
+
+    int seg, micros;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start);
+    }
+}
